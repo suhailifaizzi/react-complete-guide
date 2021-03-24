0b629e05d4356983221647576678601c2c45a50f
Suhaili Faizzi <suhailifaizzi@Suhailis-MacBook-Pro.local>
2021-03-24 16:52:44 +0800

    Use styled-components package to style components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 class App extends Component {
   // state is reserve word to define property in Component. Should hold Javascript Object
@@ -48,12 +63,7 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+
     };
 
     let persons = null
@@ -73,6 +83,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      }
     }
 
     // let classes = ['red', 'bold'].join (' '); // Generate string of red and bold to assign to class
@@ -90,10 +104,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React App.</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-          {persons}
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+          Toggle Person
+        </StyledButton>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
